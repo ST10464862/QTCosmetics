bd46f8d047d56fe85fa983d15c50783dfb1edcb5
ST10464862 <ST10464862@rcconnect.edu.za>
2025-11-19 21:02:29 +0200

    2102

--- js/script.js
+++ js/script.js
@@ -50,3 +50,49 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
+
+// Search function for index page
+function filterList() {
+ var input, filter, ul, li, i, txtValue;
+ input = document.getElementById('searchInput');
+ filter = input.value.toUpperCase();
+ ul = document.getElementById('myList');
+ li = ul.getElementsByTagName('li');
+
+ // Show the list if there's search input, otherwise hide it
+ if (filter.length > 0) {
+   ul.style.display = 'block';
+ } else {
+   ul.style.display = 'none';
+ }
+
+ // Loop through all list items, and hide those who don't match the search query
+ for (i = 0; i < li.length; i++) {
+   txtValue = li[i].textContent || li[i].innerText;
+   if (txtValue.toUpperCase().indexOf(filter) > -1) {
+     li[i].style.display = ""; // Show matching items
+   } else {
+     li[i].style.display = "none"; // Hide non-matching items
+  
+
+  // 1. Get the list item where you want to place the link
+  const listItem = document.getElementById('myListItem1');
+
+  // 2. Create the <a> element
+  const newLink = document.createElement('a');
+
+  // 3. Set the 'href' (the destination URL)
+  newLink.href = 'Perfumes.html';
+
+  // 4. (Optional) Set the 'target' to open in a new tab
+  newLink.target = '_blank';
+
+  // 5. Create the text content for the link
+  const linkText = document.createTextNode('View Product');
+
+  // 6. Append the text content to the link element
+  newLink.appendChild(linkText);
+
+   }
+ }
+}
